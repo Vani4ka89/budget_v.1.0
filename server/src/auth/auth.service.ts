37677a30d44bc2192b22a476ddfa144c88fd95a7
vanko <ivan.tym4ak@gmail.com>
2023-12-28 00:39:10 +0200

    added register and login user

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,26 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserService } from '../user/user.service';
+import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { IUser } from '../types/user.type';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+
+  constructor(private readonly userService: UserService, private readonly jwtService: JwtService) {
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(email);
+    const matchPassword = await argon2.verify(user.password, password);
+
+    if (user && matchPassword) {
+      return user;
+    }
+    throw new UnauthorizedException('User or password are incorrect!');
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async login(user: IUser) {
+    const { id, email } = user;
+    const token = this.jwtService.sign({ id: user.id, email: user.email});
+    return {
+      id,
+      email,
+      token,
+    };
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+  // create(createAuthDto: CreateAuthDto) {
+  //   return 'This action adds a new auth';
+  // }
+  //
+  // findAll() {
+  //   return `This action returns all auth`;
+  // }
+  //
+  // findOne(id: number) {
+  //   return `This action returns a #${id} auth`;
+  // }
+  //
+  // update(id: number, updateAuthDto: UpdateAuthDto) {
+  //   return `This action updates a #${id} auth`;
+  // }
+  //
+  // remove(id: number) {
+  //   return `This action removes a #${id} auth`;
+  // }
 }
--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,13 +1,25 @@
-import {Module} from '@nestjs/common';
-import {UserService} from './user.service';
-import {UserController} from './user.controller';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {User} from "./entities/user.entity";
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    controllers: [UserController],
-    providers: [UserService],
+  imports: [TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '30m' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {
 }
